4ab6a4db4373187c850100262492358fcd4e48c0
Kelsey Brown <kbrown55347@gmail.com>
2021-12-11 14:53:29 -0600

    setup Select Genre dropdown menu and populate with genres from db in AddMovieForm

--- src/components/AddMovieForm/AddMovieForm.jsx
+++ src/components/AddMovieForm/AddMovieForm.jsx
@@ -1,11 +1,14 @@
 import { useDispatch, useSelector } from 'react-redux';
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 
-function AddMovie () {
+function AddMovie() {
 
     const dispatch = useDispatch();
     // make genres reducer accessible here
     const genres = useSelector(store => store.genres)
+
+    // create local state for genre choice
+    let [genreId, setGenreId] = useState(0);
 
     // get genres on page load
     useEffect(() => {
@@ -24,10 +27,25 @@ function AddMovie () {
     return (
         <div>
             <form>
-                <input type='text' placeholder='Title'/>
-                <input type='text' placeholder='Poster Image'/>
-                <textarea type='text' placeholder='Description'/>
+                <input type='text' placeholder='Title' />
+                <input type='text' placeholder='Poster Image' />
+                <textarea type='text' placeholder='Description' />
+                {/* drop down with database genres */}
+                <select value={genreId}
+                    onChange={(evt) => setGenreId(evt.target.value)}>
 
+                    <option disabled value='0'>
+                        Select Genre
+                    </option>
+                    {genres.map((genre) => {
+                        return (
+                            <option key={genre.id} value={genre.id}>
+                                {genre.name}
+                            </option>
+                        );
+                    })}
+                </select>
+                
             </form>
         </div>
     );
