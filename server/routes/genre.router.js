38107a71078a89aa55fc7d64f5202f34e0ff1c93
Kelsey Brown <kbrown55347@gmail.com>
2021-12-11 14:47:01 -0600

    wired and tested /api/genre router to GET genres from db

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -1,11 +1,21 @@
+const { Popper } = require('@material-ui/core');
 const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
-  // Add query to get all genres
   console.log('in /api/genre GET')
-  res.sendStatus(500)
+  // Add query to get all genres
+  const sqlText = `SELECT * FROM "genres";`
+  pool.query(sqlText)
+    .then((dbRes) => {
+      res.send(dbRes.rows);
+      console.log(dbRes.rows);
+    })
+    .catch((dbErr) => {
+      console.error(dbErr);
+      res.sendStatus(500);
+    })
 });
 
 module.exports = router;
