e55eee1f4b4fd35301950ef81c5be7aad9002700
Kelsey Brown <kbrown55347@gmail.com>
2021-12-12 12:26:31 -0600

    added css styling to DetailsView

--- src/components/DetailsView/DetailsView.jsx
+++ src/components/DetailsView/DetailsView.jsx
@@ -1,46 +1,40 @@
 import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+// import CSS
+import './DetailsView.css';
 
 function DetailsView() {
 
-    const history = useHistory();
     // access detailsReducer
     const detailsReducer = useSelector(store => store.detailsReducer);
-
-    // function to bring user back to movies list page on click
-    const handleButtonClick = () => {
-        history.push('/');
-    }
     
     return (
-        <div>
-            <h2>Movie Details</h2>
-            <div>
+        <main>
+            <h1 className="details_title">Movie Details</h1>
+            <p className="directions">Click 'Home' in the menu bar above to go back to the Movie List.</p>
+                
+                <h3>{detailsReducer[0].title}</h3>
+                <div className="details_image">
+                    <img
+                        // only need to display poster once 
+                        src={detailsReducer[0].poster}
+                        alt='Movie Poster' />
+                </div>
+
                 {/* only need to display title once */}
-                <p>Movie Title: {detailsReducer[0].title}</p>
-                <br></br>
-                <img
-                    // only need to display poster once 
-                    src={detailsReducer[0].poster}
-                    alt='Movie Poster'
-                />
-                <ul>
-                    {/* map through reducer to display each 
-                    genre associated with movie */}
-                    {detailsReducer.map(genre => (
-                        <li key={genre.id}>
-                            {/* display genre name */}
-                            {genre.name}
-                        </li>
-                    ))}
-                </ul>
-                {/* only need to display description once */}
-                <p>Movie Description: {detailsReducer[0].description}</p>
-            </div>
+                <div className="description_content">
+                    <p><b>Genres: </b></p>
+                        {/* // map through detailsReduce and display each genre name */}
+                        {detailsReducer.map(genre => (
+                            <div key={genre.id}>
+                                {genre.name}
+                            </div>
+                        ))}
 
-            <button onClick={handleButtonClick}>See Movies List</button>
+                    {/* only need to display description once */}
+                    <p><b>Description:</b> {detailsReducer[0].description}</p>
+                </div> 
 
-        </div>
+        </main>
     );
 } // end DetailsView
 
